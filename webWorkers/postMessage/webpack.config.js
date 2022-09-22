a0c36d7eae2ffd9cb31d415006a431514b020025
Yves Gurcan <gurcan.yves+github@gmail.com>
2022-09-22 10:58:51 -0700

    Fixed path, maybe

--- webWorkers/postMessage/webpack.config.js
+++ webWorkers/postMessage/webpack.config.js
@@ -1,63 +1,63 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const { TARGET } = process.env;
-const isProduction = TARGET === 'build';
+const isProduction = TARGET === "build";
 console.log({ TARGET });
 
 const plugins = [
-    new HtmlWebPackPlugin({
-        template: 'src/index.html',
-    }),
+  new HtmlWebPackPlugin({
+    template: "src/index.html",
+  }),
 ];
 
 if (isProduction) {
-    plugins.unshift(new CleanWebpackPlugin());
+  plugins.unshift(new CleanWebpackPlugin());
 }
 
 module.exports = () => ({
-    output: {
-        filename: '[name].[hash].js',
-        hashDigestLength: 4,
-        publicPath: isProduction ? 'dist/' : '',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+  output: {
+    filename: "[name].[hash].js",
+    hashDigestLength: 4,
+    publicPath: isProduction ? "" : "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[local]_[hash:base64]",
+              sourceMap: true,
             },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: '[local]_[hash:base64]',
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: '[local]_[hash:base64]',
-                            sourceMap: true,
-                        },
-                    },
-                ],
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[local]_[hash:base64]",
+              sourceMap: true,
             },
+          },
         ],
-    },
-    plugins,
-    devtool: 'cheap-module-eval-source-map',
+      },
+    ],
+  },
+  plugins,
+  devtool: "cheap-module-eval-source-map",
 });
